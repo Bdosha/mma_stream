251b7db9c4aeb5bef6e38a3edf5eb4f6da4835c1
Bdosha <124150146+Bdosha@users.noreply.github.com>
2023-05-04 17:40:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Text, Command
 from aiogram.types import (CallbackQuery, InlineKeyboardButton,
                            InlineKeyboardMarkup, Message)
-
+import codecs
 
 BOT_TOKEN: str = '5578313172:AAHM5DYy6vNQJGdELh3mgYoc2L_UUPYVrDM'
-
+f = codecs.open("text.txt", "r", "utf-8")
+main_text = f.read() 
+f.close()
+print(main_text)
 
 bot: Bot = Bot(BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
-
+admins = ['DmirtyB', 'bdosha06']
 chanels = ['aboutmma_ru', 'UFCMMA00']
 
 chan_1 =  InlineKeyboardButton(
@@ -33,7 +36,8 @@ keyboard = InlineKeyboardMarkup(
 
 @dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
-    await message.answer(text='🥊Добро пожаловать🤗\n\nЯ БОТ😎, для просмотра ПРЯМОЙ ТРАНСЛЯЦИИ UFC 288🥊 необходимо подписаться на два ресурса:👇', 
+    
+    await message.answer(text=main_text, 
                          reply_markup=keyboard)
 
 @dp.callback_query(Text(text='get'))
@@ -52,6 +56,15 @@ async def notif_turn_on(callback: CallbackQuery):
             return
     print()
     await callback.message.edit_text(text=f'Вот ссылка на трансляцию! https://t.me/UFCMMA0000')
+
+@dp.message()
+async def message(message: Message):  #
+    if message.from_user.username in admins:
+        global main_text
+        main_text = message.text
+        await message.answer(text='Текст успешно изменен')
+        with codecs.open("text.txt") as temp:
+            temp.write(message.text)
 
 if __name__ == '__main__':
     dp.run_polling(bot)  # Запуск бота
